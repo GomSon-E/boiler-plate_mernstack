ab02b5f184ea2b6bbb522fb240f65103156c4610
Gomsonixx <gomsonixx@gmail.com>
2021-08-23 22:59:33 +0900

    Add Sign Up route

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const app = express()
 const port = 5000
+const bodyParser = require('body-parser')
+const { User } = require('./models/User')
 
 const mongoose = require('mongoose')
 mongoose.connect('mongodb+srv://gomsoni:gomsoni1234@boilerplate.qc5dy.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', {
@@ -8,9 +10,25 @@ mongoose.connect('mongodb+srv://gomsoni:gomsoni1234@boilerplate.qc5dy.mongodb.ne
 }).then(() => console.log('MongoDB Connected...'))
   .catch(err => console.log(err))
 
+app.use(bodyParser.urlencoded({extended: true}))
+app.use(bodyParser.json())
 
+// 기본 route
 app.get('/', (req, res) => {
   res.send('Hello World!')
+})
+
+//회원가입 route
+app.post('/register', (req, res) => {
+  // 회원가입할 때 필요한 정보를 client에서 가져옴
+  const user = new User(req.body)
+  // 가져온 정보를 데이터베이스에 저장
+  user.save((err, userInfo) => {
+    if (err) return res.json({ success: fasle, err })
+    return res.status(200).json({
+      success: true
+    })
+  })
 })
 
 app.listen(port, () => {
